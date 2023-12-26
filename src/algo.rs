9a6931be8d2bffdd5a8f33589ac09a2bc2ccdd14
absarrahman <raman040635@gmail.com>
2023-12-26 21:51:42 +0600

    Implement binary search
    Signed-off-by: absarrahman <raman040635@gmail.com>

--- src/algo.rs
+++ src/algo.rs
@@ -9,3 +9,38 @@ where
     }
     return false;
 }
+
+pub fn binary_search<T>(arr: &[T], target_value: T) -> bool
+where
+    T: PartialEq + Ord + Clone,
+{
+    let low = 0;
+    let high = arr.len();
+
+    return recursive_binary_search(arr, target_value, &low, &high);
+}
+
+fn recursive_binary_search<T>(
+    arr: &[T],
+    target_value: T,
+    low: &usize,
+    high: &usize,
+) -> bool
+where
+    T: PartialEq + Ord,
+{
+    if low >= high {
+        return false;
+    }
+
+    let mid = *low + (*high - *low) / 2;
+    let value = &arr[mid];
+
+    if value == &target_value {
+        return true;
+    } else if value > &target_value {
+        return recursive_binary_search(arr, target_value, low, &mid);
+    } else {
+        return recursive_binary_search(arr, target_value, &(mid + 1), high);
+    }
+}
--- src/test.rs
+++ src/test.rs
@@ -7,5 +7,12 @@ mod tests {
         assert_eq!(algo::linear_search(&arr, 10), true);
         assert_eq!(algo::linear_search(&arr, 11), false);
     }
+
+    #[test]
+    fn binary_search_test() {
+        let arr = [1, 5, 3, 6, 8, 10];
+        assert_eq!(algo::binary_search(&arr, 10), true);
+        assert_eq!(algo::binary_search(&arr, 11), false);
+    }
 }
 
